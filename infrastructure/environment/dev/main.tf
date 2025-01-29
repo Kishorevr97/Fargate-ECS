24086ca3a24276d33f3129610d4798ea30ad2120
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 20:52:53 +0530

    Update main.tf

--- infrastructure/environment/dev/main.tf
+++ infrastructure/environment/dev/main.tf
@@ -28,7 +28,7 @@ module "ecr" {
 module "alb" {
   source = "../../modules/alb"
   lb_name = "dev-alb"
-  public_subnet_ids = module.vpc.public_subnet
+  public_subnet = module.vpc.public_subnet
   security_groups = [module.iam.ecs_sg_id]
 }
 
