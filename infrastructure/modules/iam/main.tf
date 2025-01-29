fceb79a4727223daf39e8599002224740f7a6a63
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 21:01:56 +0530

    Update main.tf

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -9,3 +9,24 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     }]
   })
 }
+
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-security-group"
+  description = "Security group for ECS tasks"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
