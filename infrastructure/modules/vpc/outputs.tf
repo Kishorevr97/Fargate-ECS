c0570e7cf54f74843d4b5b0cc467aec9dae1096d
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 21:11:33 +0530

    Update outputs.tf

--- infrastructure/modules/vpc/outputs.tf
+++ infrastructure/modules/vpc/outputs.tf
@@ -3,8 +3,12 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "public_subnets" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = aws_subnet.public[*].id
 }
 
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
+  value       = aws_subnet.private[*].id
+}
