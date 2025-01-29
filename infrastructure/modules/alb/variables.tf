5b118584c17534331d6df3eddc447d392f45a5f4
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 21:17:34 +0530

    Update variables.tf

--- infrastructure/modules/alb/variables.tf
+++ infrastructure/modules/alb/variables.tf
@@ -12,3 +12,8 @@ variable "security_groups" {
   description = "List of security group IDs for the ALB"
   type        = list(string)
 }
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs where the ALB should be deployed"
+  type        = list(string)
+}
