5dd3095c2ac5246a555ad38a20ebc8cc8bd99ee7
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 20:36:02 +0530

    Update main.tf

--- infrastructure/modules/alb/main.tf
+++ infrastructure/modules/alb/main.tf
@@ -2,5 +2,6 @@ resource "aws_lb" "main" {
   name               = var.lb_name
   internal           = false
   load_balancer_type = "application"
-  subnets           = var.empty[*].id
+  security_groups = var.security_groups
+  subnets           = var.public_subnet_id
 }
