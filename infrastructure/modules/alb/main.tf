47f9f26c02e9932a04ee21b9d3a8d2986c8573bf
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 16:40:44 +0530

    Create main.tf

---
+++ infrastructure/modules/alb/main.tf
@@ -0,0 +1,7 @@
+resource "aws_lb" "main" {
+  name               = var.lb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = var.security_groups
+  subnets           = var.public_subnet_ids
+}
