9fd1e619ba73cbbc1b7cdab5fd538a56d6585d59
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 16:02:20 +0530

    Update main.tf

--- infrastructure/environment/dev/main.tf
+++ infrastructure/environment/dev/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+module "vpc" {
+  source          = "../../modules/vpc"
+  vpc_cidr        = "10.0.0.0/16"
+  vpc_name        = "dev-vpc"
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  azs             = ["eu-north-1a", "eu-north-1b"]
+}
+
