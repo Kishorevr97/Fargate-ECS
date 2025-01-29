c30d0e082e4b42c9448f3e65c1680b4e6a368a93
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-29 19:47:45 +0530

    Update variables.tf

--- infrastructure/environment/dev/variables.tf
+++ infrastructure/environment/dev/variables.tf
@@ -27,3 +27,18 @@ variable "azs" {
   description = "List of availability zones"
   type        = list(string)
 }
+
+variable "lb_name" {
+  description = "Name of the Application Load Balancer"
+  type        = string
+}
+
+variable "public_subnet_ids" {
+  description = "List of public subnet IDs for the ALB"
+  type        = list(string)
+}
+
+variable "security_groups" {
+  description = "List of security group IDs for the ALB"
+  type        = list(string)
+}
