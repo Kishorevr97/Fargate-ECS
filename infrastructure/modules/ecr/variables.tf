a314feb5dc841687b98e0d67ee71083e339c2150
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-30 13:56:09 +0530

    Update variables.tf

--- infrastructure/modules/ecr/variables.tf
+++ infrastructure/modules/ecr/variables.tf
@@ -1,4 +1,9 @@
-variable "repo_name" {
-  description = "Name of the ECR repository"
-  type        = string
+variable "repository_name" {
+  description = "The name of the ECR repository"
+  type = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type = string
 }
