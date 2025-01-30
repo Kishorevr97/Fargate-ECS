5efbef4bbfd1192848dc9a711bb80b350601a849
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-30 13:49:59 +0530

    Update main.tf

--- infrastructure/environment/dev/main.tf
+++ infrastructure/environment/dev/main.tf
@@ -1,37 +1,42 @@
-provider "aws" {
-  aws_region = var.aws_region
-}
+
 
 module "vpc" {
-  source          = "../../modules/vpc"
-  vpc_cidr        = var.vpc_cidr
-  vpc_name        = "dev-vpc"
-  public_subnets  = var.public_subnets
-  private_subnets = var.private_subnets
-  azs             = var.azs
+  source               = "../../modules/vpc"
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  availability_zones   = var.availability_zones
+  environment         = var.environment
 }
-
 module "ecs" {
-  source = "../../modules/ecs"
-  cluster_name = "dev-cluster"
+  source                  = "../../modules/ecs"
+  public_subnet_ids       = module.vpc.public_subnet_ids
+  ecs_security_group_id   = module.iam.ecs_security_group_id
+  environment             = var.environment
+  execution_role_arn      = module.iam.ecs_execution_role_arn
+  task_role_arn           = module.iam.ecs_task_role_arn
+  ecr_patient_repo_url    = module.ecr.patient_service_repo_url
+  ecr_appointment_repo_url = module.ecr.appointment_service_repo_url
+
 }
 
 module "iam" {
-  source = "../../modules/iam"
-  vpc_id = module.vpc.vpc_id
+  source                      = "../../modules/iam"
+  execution_role_policy_arn   = var.execution_role_policy_arn
+  vpc_id                      = module.vpc.vpc_id
+  environment                 = var.environment
 }
 
 module "ecr" {
-  source = "../../modules/ecr"
-  repo_name = "dev-repo"
+  source      = "../../modules/ecr"
+  repository_name = "my-ecr-repo-${var.environment}"
+  environment = var.environment
 }
-
 
 module "alb" {
-  source            = "../../modules/alb"
-  lb_name           = "dev-alb"
-  subnets = module.vpc.public_subnet
-  security_groups   = [module.iam.ecs_sg_id]
+  source                = "../../modules/alb"
+  alb_security_group_id     = module.iam.alb_security_group_id
+  public_subnet_ids      = module.vpc.public_subnet_ids
+  vpc_id                = module.vpc.vpc_id
+  environment           = var.environment
 }
-
-
