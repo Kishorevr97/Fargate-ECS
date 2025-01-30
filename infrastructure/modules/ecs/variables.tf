15efade55b645052c44aa1740508992cc8a25cb1
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-30 13:57:09 +0530

    Update variables.tf

--- infrastructure/modules/ecs/variables.tf
+++ infrastructure/modules/ecs/variables.tf
@@ -1,4 +1,18 @@
-variable "cluster_name" {
-  description = "Name of the ECS cluster"
-  type        = string
+variable "public_subnet_ids" {
+  description = "The public subnet ID"
+  type = list(string)
 }
+
+variable "ecs_security_group_id" {
+  description = "Security group ID for ECS"
+  type = string
+}
+
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type = string
+}
+variable "execution_role_arn" {}
+variable "task_role_arn" {}
+variable "ecr_patient_repo_url" {}
+variable "ecr_appointment_repo_url" {}
