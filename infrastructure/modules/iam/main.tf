d10d967495f832ce27d893e130c06d9bfe83cf8e
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-30 21:36:14 +0530

    Update main.tf

--- infrastructure/modules/iam/main.tf
+++ infrastructure/modules/iam/main.tf
@@ -31,18 +31,35 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role" {
   policy_arn = var.execution_role_policy_arn
 }
 
-
 # Security Group for ALB
 resource "aws_security_group" "alb_security_group" {
   name        = "${var.environment}-alb-sg"
   description = "Security group for ALB"
   vpc_id      = var.vpc_id
   ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
+  }
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+    cidr_blocks = ["0.0.0.0/0"] 
 
   egress {
     from_port   = 0
@@ -58,10 +75,23 @@ resource "aws_security_group" "ecs_security_group" {
   description = "Security group for ECS tasks"
   vpc_id      = var.vpc_id
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS traffic from anywhere
+  }
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   egress {
