2f88628a5c919fdacea169b0bbbef70582681425
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-01-30 13:52:33 +0530

    Update variables.tf

--- infrastructure/environment/dev/variables.tf
+++ infrastructure/environment/dev/variables.tf
@@ -1,45 +1,36 @@
-variable "aws_region" {
-
-       description = "region where we create resource"
- }
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
 
-variable "vpc_name" {
-  description = "Name of the VPC"
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to use for subnets"
+  type        = list(string)
+  default     = ["us-west-1b", "us-west-1c"]  # Update with the zones you want to use
+}
+
+
+variable "execution_role_policy_arn" {
+  description = "IAM policy ARN for ECS execution role"
   type        = string
 }
 
-variable "public_subnets" {
-  description = "List of public subnet CIDRs"
-  type        = list(string)
-}
 
-variable "private_subnets" {
-  description = "List of private subnet CIDRs"
-  type        = list(string)
+variable "environment" {
+  description = "Environment name (e.g., dev, staging, prod)"
+  type = string
 }
-
-variable "azs" {
-  description = "List of availability zones"
-  type        = list(string)
-}
-
-variable "lb_name" {
-  description = "Name of the Application Load Balancer"
+variable "repository_name" {
+  description = "ECR repository name"
   type        = string
-}
-
-
-variable "security_groups" {
-  description = "List of security group IDs for the ALB"
-  type        = list(string)
-}
-
-variable "public_subnet_ids" {
-  description = "List of public subnet IDs where the ALB should be deployed"
-  type        = list(string)
 }
