f44de3aebdb0418ca9a07b78178bffc02d2ea66f
Kishorevr97 <107535279+Kishorevr97@users.noreply.github.com>
2025-02-05 18:02:35 +0530

    Update outputs.tf

--- infrastructure/modules/ecs/outputs.tf
+++ infrastructure/modules/ecs/outputs.tf
@@ -6,3 +6,8 @@ output "ecs_cluster_id" {
 output "ecs_task_definition_arn" {
   value = aws_ecs_task_definition.task_definition.arn
 }
+
+
+output "log_group_name" { 
+value = aws_cloudwatch_log_group.ecs_logs.name
+}
